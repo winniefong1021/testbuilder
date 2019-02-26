2b7b9ed4d505808b2ba68377df57890c82524927
Winnie Fong <winniefong21@gmail.com>
2019-02-25 21:00:52 -0800

    Fix Visa and Mastercard tests to pass

--- detectNetwork.test.js
+++ detectNetwork.test.js
@@ -65,7 +65,7 @@ describe('American Express', function () {
   // It can get annoying to keep typing the if/throw, so here is a
   // helper function to throw an error if the input statement isn't true.
   var assert = function (isTrue) {
-    if (isTrue) {
+    if (!isTrue) {
       throw new Error('Test failed');
     }
   };
@@ -82,9 +82,9 @@ describe('American Express', function () {
 describe('Visa', function () {
   // Chai is an entire library of helper functions for tests!
   // Chai provides an assert that acts the same as our previous assert.
-  // Search the documentation to figure out how to access it. 
+  // Search the documentation to figure out how to access it.
   //   http://chaijs.com/
-  var assert = chai.FILL_ME_IN;
+  var assert = chai.assert;
 
 
   it('has a prefix of 4 and a length of 13', function () {
@@ -102,39 +102,38 @@ describe('Visa', function () {
 
 describe('MasterCard', function () {
   // Chai lets you write more human-readable tests that throw helpful errors.
-  // Expect syntax is one way to do this, but there are others. 
-  // If you want to know more, check out the documentation. 
+  // Expect syntax is one way to do this, but there are others.
+  // If you want to know more, check out the documentation.
   //   http://chaijs.com/api/bdd/
   var expect = chai.expect;
 
-  it(FILL_ME_IN, function () {
+  it('has a prefix of 51 and a length of 16', function () {
     expect(detectNetwork('5112345678901234')).to.equal('MasterCard');
   });
 
-  it(FILL_ME_IN, function () {
+  it('has a prefix of 52 and a length of 16', function () {
     expect(detectNetwork('5212345678901234')).to.equal('MasterCard');
   });
 
-  it(FILL_ME_IN, function () {
+  it('has a prefix of 53 and a length of 16', function () {
     expect(detectNetwork('5312345678901234')).to.equal('MasterCard');
   });
 
-
   // You can also use should instead of expect, which changes the style
-  // slightly. It really doesn't matter which one you use - check out 
+  // slightly. It really doesn't matter which one you use - check out
   // http://chaijs.com/guide/styles/ for more info, but it's important
   // to be consistent (unlike in this file, where we use BOTH expect
-  // and should, but that's just for learning), so once you've gotten 
-  // these tests to pass using should syntax, refactor your tests to 
-  // use either expect or should, but not both. 
-  var should = chai.should();
+  // and should, but that's just for learning), so once you've gotten
+  // these tests to pass using should syntax, refactor your tests to
+  // use either expect or should, but not both.
+  // var should = chai.should();
 
   it('has a prefix of 54 and a length of 16', function () {
-    detectNetwork('5412345678901234').should.equal(FILL_ME_IN);
+    expect(detectNetwork('5412345678901234')).to.equal('MasterCard');
   });
 
   it('has a prefix of 55 and a length of 16', function () {
-    detectNetwork('5512345678901234').should.equal(FILL_ME_IN);
+    expect(detectNetwork('5512345678901234')).to.equal('MasterCard');
   })
 
 });
